41a215b73e1230fede5a19e7dd6905c2b011d63a
PedroCosta138 <pedrocosta1111.pc@gmail.com>
2023-05-02 10:40:20 +0100

    Definir o nome e versao de base de dados

--- app/src/main/java/com/example/instrumentos/BdInstrumentosOpenHelper.kt
+++ app/src/main/java/com/example/instrumentos/BdInstrumentosOpenHelper.kt
@@ -4,20 +4,19 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+private const val NOME_BASE_DADOS = "Instrumentos.db"
+private const val VERSAO_BASE_DADOS = 1
+
 class BdInstrumentosOpenHelper(
     context: Context?,
-    name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
-    version: Int
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
 
 
 
-) : SQLiteOpenHelper(context, name, factory, version) {
     override fun onCreate(p0: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
     }
 }
