ffbc1f79fd03754823d12a20f241499f04070f15
PedroCosta138 <pedrocosta1111.pc@gmail.com>
2023-05-05 10:17:09 +0100

    on delete

--- app/src/main/java/com/example/instrumentos/TabelaInstrumentos.kt
+++ app/src/main/java/com/example/instrumentos/TabelaInstrumentos.kt
@@ -8,7 +8,7 @@ class TabelaInstrumentos(db: SQLiteDatabase):TabelaBD(db,"Instrumentos") {
 
     override fun cria() {
         db.execSQL("CREATE TABLE $NOME_TABELA($CHAVE_TABELA,nome TEXT NOT NULL, tipo TEXT NOT NULL,paisOrigem TEXT NOT NULL,id_brands INTEGER NOT NULL)" +
-                ", FOREIGN KEY(id_brands) REFERENCES ${TabelaBrands.NOME_TABELA}(${BaseColumns._ID})")
+                ", FOREIGN KEY(id_brands) REFERENCES ${TabelaBrands.NOME_TABELA}(${BaseColumns._ID})ON DELETE RESTRICT")
     }
 
     companion object{
